1d06ed9d639c904ce107a1448535eadc2872bbe8
Johnny Dong <dongjiayang123@hotmail.com>
2026-02-15 13:31:51 -0700

    initial

--- features/editor/components/NoLetterSelected.tsx
+++
@@ -1,7 +0,0 @@
-export default function NoLetterSelected() {
-    return (
-        <div className="h-full w-full flex items-center justify-center">
-            <div className="text-sm text-gray-700">Select or create a letter</div>
-        </div>
-    )
-}
